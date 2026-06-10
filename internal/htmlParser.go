8f34a888d808b2b0d1cc8d7af39ef44f565f5963
umesshk <umeshkumar01439@gmail.com>
2026-06-10 16:05:15 +0530

    tree traversing

--- internal/htmlParser.go
+++ internal/htmlParser.go
@@ -9,28 +9,43 @@ import (
 	"golang.org/x/net/html/atom"
 )
 
+func Printnodes(node *html.Node) {
+
+	/* Traversing by recursion
+
+	if node.type == html.elementnode && node.dataatom == atom.a {
+		for _, a := range node.attr {
+			if a.key == "href" {
+				fmt.println(a.val)
+			}
+		}
+	}
+
+	for c := node.firstchild; c != nil; c = c.nextsibling {
+		printnodes(c)
+	}
+	*/
+	for n := range node.Descendants() {
+		if n.Type == html.ElementNode && n.DataAtom == atom.A {
+			for _, a := range n.Attr {
+
+				if a.Key == "href" {
+					fmt.Println(a.Val)
+				}
+			}
+		}
+	}
+
+}
+
 func ParseHtml(file *os.File) {
 
 	node, err := html.Parse(file)
 
 	if err != nil {
-		log.Fatal("Error Occured", err)
+		log.Fatal("error occured", err)
 		return
 	}
-
-	for n := range node.Descendants() {
-
-		if n.Type == html.ElementNode {
-			if n.DataAtom == atom.A {
-				for _, a := range n.Attr {
-					if a.Key == "href" {
-						fmt.Println(a.Val)
-					}
-				}
-
-			}
-		}
-
-	}
+	Printnodes(node)
 
 }
