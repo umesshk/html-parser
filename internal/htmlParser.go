fee22ef66a664e2419703651ccdf62e5063740c1
umesshk <umeshkumar01439@gmail.com>
2026-06-10 23:24:01 +0530

    getting text from tag

--- internal/htmlParser.go
+++ internal/htmlParser.go
@@ -4,38 +4,76 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
 
-func Printnodes(node *html.Node) {
+type Link struct {
+	Href string `json:"href"`
+	Text string `json:"text"`
+}
 
-	/* Traversing by recursion
+var linkTags []Link
 
-	if node.type == html.elementnode && node.dataatom == atom.a {
-		for _, a := range node.attr {
-			if a.key == "href" {
-				fmt.println(a.val)
+func getText(n *html.Node) string {
+
+	var text string
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.TextNode {
+			data := strings.TrimSpace(c.Data)
+			if data != "" {
+				text += data
 			}
 		}
 	}
+	return text
+}
 
-	for c := node.firstchild; c != nil; c = c.nextsibling {
-		printnodes(c)
-	}
-	*/
-	for n := range node.Descendants() {
-		if n.Type == html.ElementNode && n.DataAtom == atom.A {
-			for _, a := range n.Attr {
+func Printnodes(node *html.Node) {
 
-				if a.Key == "href" {
-					fmt.Println(a.Val)
+	// Traversing by recursion/DFS
+
+	/*
+
+		if node.type == html.elementnode && node.dataatom == atom.a {
+			for _, a := range node.attr {
+				if a.key == "href" {
+					fmt.println(a.val)
 				}
 			}
 		}
-	}
 
+		for c := node.firstchild; c != nil; c = c.nextsibling {
+			printnodes(c)
+		}
+	*/
+
+	for n := range node.Descendants() {
+		var link string
+		var text string
+		if n.Type == html.ElementNode && n.DataAtom == atom.A {
+
+			for _, a := range n.Attr {
+
+				if a.Key == "href" {
+					val := strings.TrimSpace(a.Val)
+					if val != "" {
+						link = val
+					}
+
+					break
+				}
+			}
+
+			text = getText(n)
+
+			linkTags = append(linkTags, Link{Href: link, Text: text})
+		}
+
+	}
 }
 
 func ParseHtml(file *os.File) {
@@ -47,5 +85,10 @@ func ParseHtml(file *os.File) {
 		return
 	}
 	Printnodes(node)
+
+	for _, l := range linkTags {
+		fmt.Println("href : ", l.Href)
+		fmt.Println("Text: ", l.Text)
+	}
 
 }
